47293b093cbdba91069f6b0af4079adcc68167d5
aredpig <bofeng@ualberta.ca>
2020-02-06 19:18:07 -0700

    Update Shape.java

--- app/src/main/java/com/example/team/Shape.java
+++ app/src/main/java/com/example/team/Shape.java
@@ -3,4 +3,8 @@ package com.example.team;
 public abstract class Shape {
     private int x;
     private int y;
+<<<<<<< Updated upstream
+=======
+    String color = "yellow";
+>>>>>>> Stashed changes
 }
